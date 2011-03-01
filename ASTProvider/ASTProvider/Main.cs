8f22e9bac0128ca5c350f51f591f34f3c0cc8649
Davy Landman <davy.landman+uva@gmail.com>
2011-03-01 19:16:06 +0100

    Added initial structure for code generation

--- ASTProvider/ASTProvider/Main.cs
+++ ASTProvider/ASTProvider/Main.cs
@@ -7,15 +7,43 @@ using System.Net;
 using System.IO;
 using Landman.Rascal.CSharp.Profobuf;
 using Google.ProtocolBuffers;
+using ICSharpCode.NRefactory.CSharp;
 
 
 namespace Landman.Rascal.CSharp.ASTProvider
 {
-	class MainClass
+	partial class MainClass
 	{
+		static partial void GenerateNode(ICSharpCode.NRefactory.CSharp.AstNode root, Landman.Rascal.CSharp.Profobuf.AstNode.Builder result);
+		
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			var server = new TcpListener(IPAddress.Loopback, 5556);
+			server.Start();
+			while (true)
+			{
+				var newClient = server.AcceptTcpClient();
+				Console.WriteLine("Got new client.");
+				using (var clientStream = newClient.GetStream())
+				{
+					var request = CSharpParseRequest.ParseDelimitedFrom(clientStream);
+					Console.WriteLine("Starting collecting AST for:\n");
+					Console.WriteLine(request.Filename);
+					var parser = new CSharpParser();
+					var csFile = new FileStream(request.Filename, FileMode.Open);
+					var cu = parser.Parse(csFile);
+					var resultBuilder = CSharpParseResult.CreateBuilder();
+					foreach (var child in cu.Children) {
+						var builder = Landman.Rascal.CSharp.Profobuf.AstNode.CreateBuilder();
+						GenerateNode(child, builder);
+						resultBuilder.AddResult(builder);
+					}
+					resultBuilder.Build().WriteDelimitedTo(clientStream);
+					Console.WriteLine("\nFinished");
+				}
+
+			}
+
 		}
 	}
 }
