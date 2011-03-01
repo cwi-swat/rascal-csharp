5700e913f2863e30865308d70c1bc6cef1050100
Davy Landman <davy.landman+uva@gmail.com>
2011-03-01 18:56:16 +0100

    Imported first csharp project

---
+++ ASTProvider/ASTProvider/Main.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using Landman.Rascal.CSharp.Profobuf;
+using Google.ProtocolBuffers;
+
+
+namespace Landman.Rascal.CSharp.ASTProvider
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+		}
+	}
+}
+
